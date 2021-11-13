7dd976102bc75f1674bb1285a6f85cdd82da054e
rashedul360m <rashedul360rs@gmail.com>
2021-11-13 22:56:39 +0600

    not found

--- src/Components/Pages/NotFound/NotFound.js
+++ src/Components/Pages/NotFound/NotFound.js
@@ -1,14 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import notFoundimage from "../../../images/notFoundimg.svg";
 const NotFound = () => {
   return (
     <div>
       {/* not found image  */}
-      <img
-        src={notFoundimage}
-        className="w-100 mx-auto img-fluid"
-        alt="image"
-      />
+      <div className="text-center mt-5">
+        <Link to="/">
+          <button className="btn btn-danger w-50 mb-5">Go to home</button>
+        </Link>
+        <img src={notFoundimage} className="mx-auto img-fluid" alt="image" />
+      </div>
     </div>
   );
 };
